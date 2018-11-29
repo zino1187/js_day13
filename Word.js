66da83c5dbd2534b378048b392c55056b449a414
zino1187 <zino1187@gmail.com>
2018-11-29 10:49:59 +0900

    no message

--- Word.js
+++ Word.js
@@ -3,17 +3,20 @@ class Word{
 	constructor(container,text,x,y){
 		//span 태그는 인라인 속성이므로, 너비가 컨텐츠만큼
 		//만 확보된다
+		this.x=x;
+		this.y=y;
 		this.span=document.createElement("span");
 		this.span.innerText=text;
 		this.span.style.fontSize="20px";
 		this.span.style.fontWeight="bold";
 		this.span.style.position="absolute";
-		this.span.style.left=x+"px";
-		this.span.style.top=y+"px";
+		this.span.style.left=this.x+"px";
+		this.span.style.top=this.y+"px";
 
 		container.appendChild(this.span);
 	}
 	move(){
-
+		this.y+=5;//기존 y값을 5씩 누적!!
+		this.span.style.top=this.y+"px";
 	}
 }

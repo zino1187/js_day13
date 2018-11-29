ad771828cc73be2be90d09687aa5bc2e4e6e314b
zino1187 <zino1187@gmail.com>
2018-11-29 10:21:33 +0900

    no message

--- Word.js
+++ Word.js
@@ -1,8 +1,15 @@
 /*게임에 사용될 단어를 정의한다!!*/
 class Word{
-	constructor(){
-		this.div=document.createElement("div");
+	constructor(container,text){
+		//span 태그는 인라인 속성이므로, 너비가 컨텐츠만큼
+		//만 확보된다
+		this.span=document.createElement("span");
+		this.span.innerText=text;
+		this.span.style.fontSize="20px";
+		this.span.style.fontWeight="bold";
+		container.appendChild(this.span);
 	}
 	move(){
+
 	}
 }
